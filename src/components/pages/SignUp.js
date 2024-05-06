185f11d099922f6620e90864e27b2074b7145030
MeTheGreat1474 <ahmadtaufiq1474@gmail.com>
2024-05-07 01:10:41 +0800

    updated to work properly for sign up

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -7,32 +7,40 @@ import api from "../../api/axiosConfig";
 
 function SignUp() {
 
+    const navigate = useNavigate();
+
     // States for registration
-    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [dob, setDOB] = useState("");
+    // const [dob, setDOB] = useState("");
     const [address, setAddress] = useState("");
 
     // States for checking the errors
     const [submitted, setSubmitted] = useState(false);
     const [error, setError] = useState(false);
 
-    const [nameValid, setNameValid] = useState(true);
+    const [usernameValid, setUsernameValid] = useState(true);
     const [emailValid, setEmailValid] = useState(true);
     const [passwordValid, setPasswordValid] = useState(true);
-    const [dobValid, setDobValid] = useState(true);
+    // const [dobValid, setDobValid] = useState(true);
     const [addressValid, setAddressValid] = useState(true);
 
-    const signup = async (name, email, password, dob, address) => {
-        const response = await api.post(`/Account/signup`, {name, email, password, dob, address});
-        console.log(`Name: ${name}, Email: ${email}, Password: ${password}, DOB: ${dob}, Address: ${address}`)
-        return response.data;
+    const signup = async (username, email, password, address) => {
+        try {
+            console.log(`1:  Name: ${username}, Email: ${email}, Password: ${password}, Address: ${address}`)
+            const response = await api.post(`/Account/signUp?username=${username}&email=${email}&password=${password}&address=${address}`);
+            console.log(`2:  Name: ${username}, Email: ${email}, Password: ${password}, Address: ${address}`)
+            return response.data;
+        } catch (error) {
+            console.error('Error during signup:', error);
+            return null;
+        }
     }
 
-    // Handling the name change
+    // Handling the username change
     const handleName = (e) => {
-        setName(e.target.value);
+        setUsername(e.target.value);
         setSubmitted(false);
     };
 
@@ -48,10 +56,10 @@ function SignUp() {
         setSubmitted(false);
     };
 
-    const handleDOB = (e) => {
-        setDOB(e.target.value);
-        setSubmitted(false);
-    };
+    // const handleDOB = (e) => {
+    //     setDOB(e.target.value);
+    //     setSubmitted(false);
+    // };
 
     const handleAddress = (e) => {
         setAddress(e.target.value);
@@ -63,19 +71,19 @@ function SignUp() {
         e.preventDefault();
 
         // handle the error messages below the input form
-        setNameValid(name !== "");
+        setUsernameValid(username !== "");
         setEmailValid(email !== "");
         setPasswordValid(password !== "");
-        setDobValid(dob !== "");
+        // setDobValid(dob !== "");
         setAddressValid(address !== "");
 
-        if (nameValid===false || emailValid===false || passwordValid===false || dobValid===false || addressValid===false)
+        if (usernameValid===false || emailValid===false || passwordValid===false || addressValid===false)
         {
             setError(true);
             console.log('error')
         }
         else {
-            const response = await signup(name, email, password, dob, address);
+            const response = await signup(username, email, password, address);
             console.log(`Response: ${response}`);
             if (response) {
                 navigate('/dashboard');
@@ -94,8 +102,8 @@ function SignUp() {
                 </div>
                 <div className="sign-up-container">
                     <form className='sign-up-form' onSubmit={handleOnSubmit} >
-                        <SignUpForm label='Username' onChange={handleName} className='input' value={name} type='text'/>
-                        <div className={`error-message ${nameValid ? "" : "visible"}`}>* Please enter your name</div>
+                        <SignUpForm label='Username' onChange={handleName} className='input' value={username} type='text'/>
+                        <div className={`error-message ${usernameValid ? "" : "visible"}`}>* Please enter your name</div>
 
                         <SignUpForm label='Email' onChange={handleEmail} className='input' value={email} type='email'/>
                         <div className={`error-message ${emailValid ? "" : "visible"}`}>* Please enter your email</div>
@@ -103,8 +111,8 @@ function SignUp() {
                         <SignUpForm label='Password' onChange={handlePassword} className='input' value={password} type='password'/>
                         <div className={`error-message ${passwordValid ? "" : "visible"}`}>* Please enter your password</div>
 
-                        <SignUpForm label='Date of Birth' onChange={handleDOB} className='input' value={dob} type='date'/>
-                        <div className={`error-message ${dobValid ? "" : "visible"}`}>* Please enter your date of birth</div>
+                        {/*<SignUpForm label='Date of Birth' onChange={handleDOB} className='input' value={dob} type='date'/>*/}
+                        {/*<div className={`error-message ${dobValid ? "" : "visible"}`}>* Please enter your date of birth</div>*/}
 
                         <SignUpForm label='Address' type='textarea' rows='10' cols='30' onChange={handleAddress} className={`input ${addressValid ? "" : "error"}`} value={address}/>
                         <div className={`error-message ${addressValid ? "" : "visible"}`}>* Please enter your address</div>
