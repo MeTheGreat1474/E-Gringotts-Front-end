216a99faadf01e89b9bd5aab35f84b61a0fa5f08
MeTheGreat1474 <ahmadtaufiq1474@gmail.com>
2024-06-02 01:25:39 +0800

    enable add to desktop

--- src/App.js
+++ src/App.js
@@ -23,6 +23,16 @@ function App() {
 
     //TODO: CREATE APP DEPLOYMENT FOR WEBSITE
 
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+
   return (
     <>
       <Router>
